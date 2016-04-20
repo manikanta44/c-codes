eca1d47b84ba4cfcbe0ce3995c589c58289db712
manikanta44 <rockstarmanikanta04@gmail.com>
2016-04-20 02:35:24 -0700

    Update string.c

--- string.c
+++ string.c
@@ -8,8 +8,25 @@ void main()
     scanf("%s",a);
     printf("enter limits");
     scanf("%d%d",&n,&m);
+if(n<m)
+
+{
     for(i=n;i<m;i++)
     {
       printf("%c",a[i]);
-    }getch();
+    }
+}
+else
+{
+     for(i=m;i<n;i++)
+    {
+      printf("%c",a[i]);
+    }
+}
+
+
+
+getch();
+
+    
 }
