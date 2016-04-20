285d156d1a4fc37be546be502adb8a7dec369bf1
manikanta44 <rockstarmanikanta04@gmail.com>
2016-04-20 02:15:17 -0700

    Create string.c

---
+++ string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[100];
+    clrscr();
+    printf("enter characters");
+    scanf("%s",a);
+    printf("enter limits");
+    scanf("%d%d",&n,&m);
+    for(i=n;i<m-1;i++)
+    {
+      printf("%c",a[i]);
+    }getch();
+}
